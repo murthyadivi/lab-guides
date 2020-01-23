12acdc28c042f4a19ff2e8b7c05798546d0936b3
murthyadivi <murthy.adivirk@gmail.com>
2020-01-23 17:57:10 +0530

    Add files via upload

---
+++ py3.py
@@ -0,0 +1,20 @@
+"This program describles simple variable assignment"
+
+word_count = 120 # assigning an integer value
+temperature = 98.3 # assigning a float value
+py = "Hello World" # assinging a string value
+
+print (word_count)
+print (temperature)
+print (py)
+
+x = 2
+y = 3
+
+x%=y
+x
+
+x = False
+y = False
+z = x and y
+z
